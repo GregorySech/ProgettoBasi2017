cc58a09f122aa9004da4ff866218c5aea284da1d
gregory <gregory@gregory-Lenovo-ideapad-Y700-15ISK>
2017-06-13 14:36:14 +0200

    Primo commit

---
+++ ProgettoBasi2017/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Connection informations to Database
+ *
+ * @author gregory
+ */
+class connection {
+
+    public static $dbname = "a2016u125";
+    public static $dbuser = "a2016u125";
+    public static $dbpassword = "7DJ7LpV.";
+
+    public static function getDBHost() {
+        return 'pgsql:host=dblab.dsi.unive.it;port=5432;dbname=' . (connection::$dbname);
+    }
+
+}
